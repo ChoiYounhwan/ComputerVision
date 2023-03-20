89003a5a7ced048805cb65b4ea35579e62a2511e
Choiyounhwan <cyhwan0816@gmail.com>
2023-03-20 17:36:47 +0900

    My Commit4

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2
 
 # 영상 소스 열기
-src = cv2.imread('c:/photo/picture01.jpg')
+src = cv2.imread('c:/photo/picture88.jpg')
 
 
 
@@ -9,6 +9,8 @@ src = cv2.imread('c:/photo/picture01.jpg')
 dst1 = cv2.cvtColor(src, cv2.COLOR_RGB2GRAY)
 dst2 = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
 
+dst3 = src.copy()
+_, dst3 = cv2.threshold(dst3, 127, 255, cv2.THRESH_BINARY)
 
 
 
